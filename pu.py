de8295ec5b7351e3f38b80f8990839b8360f41d8
Toktobekov Kylymbek <kylymbekkemin@gmail.com>
2023-04-15 14:06:48 +0600

    ggff

--- pu.py
+++ pu.py
@@ -1,6 +1,6 @@
 import websocket
-import datetime
 import json
+import requests
 
 temp=[ 'rfer']
 dt="rvbrtvrtv"
@@ -11,40 +11,16 @@ def on_message(ws, message):
         data1=last['data']
         if "rr" in data1:
             rr=data1["rr"]
-            if dt!=rr["dt"]:
-                print("c = "+str(rr["c"])+" :: v = "+str(rr["v"])+" :: sv = "+str(data1["sv"])+" :: tv = "+str(data1["tv"]))
-                file=open("number1111111111.txt","a")
-                file1=open("number222222222.txt","a")
-                file2=open("number1111.txt","a")
-                file3=open("number.txt","a")
-                file3.write(str(rr["c"])+"\n")
-                file3.close()
-
-                streamKline11()
-
+            if dt!=rr["dt"]:                
                 if int(rr["c"])<10 :
                     print(str(rr["c"]))
-                    file.write(str(rr["c"])+"\n")
-                    file2.write(str(rr["c"])+"\n")
-                    file1.write(str(rr["c"])+" "+str(rr["c"])+"\n")
-
+                    send_Telegram(str(str(rr["c"])+" "+str(rr["c"])))
                 else:
                     nummm=str(rr["c"])
                     a1=nummm[0:1]
                     a2=nummm[1:2]
                     print(a1+" "+a2)
-             
-                    file.write(str(int(a1)+int(a2))+"\n")
-                    file2.write(str(int(a1)+int(a2))+"\n")
-
-                    file1.write(str(int(a1)+int(a2))+" "+str(rr["c"])+"\n")
-
-                    
-                file.close()
-                file1.close()
-                file2.close()
-
-                
+                    send_Telegram(str(str(int(a1)+int(a2))+" "+str(rr["c"])))                
                 dt=rr["dt"]
                 
 def on_error(ws, error):
@@ -64,20 +40,19 @@ def streamKline():
 
     ws.run_forever()
     
-def streamKline11():
-    previous_numbers = []
-    f=open("number.txt")
-    file1=open("number555555555.txt","w")
+def send_Telegram(text: str):
+    token = "5741206806:AAFHkbFfw3EkRp0aXRfWAkWLeAQztwHYtXc"
+    url = "https://api.telegram.org/bot"
+    channel_id = "-1001814924725"
+    url += token
+    method = url + "/sendMessage"
 
-    for line in f:
-        previous_numbers.append(int(line))
-    for i in range(0,len(previous_numbers)-1,1):
-        if int(previous_numbers[i])>int(previous_numbers[i+1]):
-            file1.write(str(int(previous_numbers[i])-int(previous_numbers[i+1]))+"\n")
-        else:
-            file1.write(str(int(previous_numbers[i])-int(previous_numbers[i+1]))+"\n")
+    r = requests.post(method, data={
+         "chat_id": channel_id,
+         "text": text
+          })
 
-    f.close()
-    file1.close()
+    if r.status_code != 200:
+        raise Exception("post_text error")
 
 streamKline()
