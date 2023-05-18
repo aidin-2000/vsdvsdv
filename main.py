2404d0f6ba8cc84121aebc32227c508871faf133
Toktobekov Kylymbek <kylymbekkemin@gmail.com>
2023-05-19 01:10:21 +0600

    g

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ dt1 = "tempdate"
 srez="dd"
 european_sequence = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
 result=[]
+tempfac=[ 'rfer']
+tempfac2=['ff']
+tempfac1=['fssf']
+tempfac007=[]
 def on_error(ws,error):
     print(str("Проблема с подключением к интернету! Подробнее : " + str(error)))
     print("Попытка подключится заново!")
@@ -35,6 +39,16 @@ def on_message(ws,message):
                 print("Выпало число = " + str(c))
                 send_Telegram(str(c))
                 dt = rr["dt"]
+    if last["type"]=="factors":
+        datafacfac=last['data']
+        if datafacfac[len(datafacfac)-1]["dt"] != tempfac[0]:
+            tempfac[0]=datafacfac[len(datafacfac)-1]["dt"]
+            c=datafacfac[len(datafacfac)-1]["v"]
+            ccc=str(c)
+            tempfac007.append(ccc)
+            if len(tempfac007)==150:
+                send_Telegram(str(tempfac007))
+                tempfac007.clear()
 
 
 def send_Telegram(text: str):
